491480c7c76c0414eaa74ce47b55451de49dedac
Raphael Taibi <raphael.taibi@epitech.eu>
2025-11-22 20:49:40 +0100

    feat: :sparkles: login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,75 @@
+'use client';
+
+import React from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card } from "cs-tools-kit";
+
 export default function LoginPage() {
+
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState<string | null>(null);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await signIn("credentials", {
+      redirect: false,
+      username,
+      password,
+    });
+
+    if (res?.error) {
+      setError("Username or password is incorrect");
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-6">
-      <p className="text-lg text-gray-700">LOGIN</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+
+        <Card 
+          variant="elevated"
+          header = {<h2 className="text-2xl font-semibold text-center mb-6">Admin Login</h2>}
+          className="w-full max-w-md"
+        >
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <Input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+
+            <div>
+              <Input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+
+            <div className="flex justify-center">
+              <Button type="submit" size="lg" className="px-8 py-2 text-base hover:bg-stone-800 hover:text-white" variant="secondary">
+                Login
+              </Button>
+            </div>
+          </form>
+        </Card>
+      </div>
     </div>
   );
 }
